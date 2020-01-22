f05f52917ecf4c7e9b726c7324b5a73d3958af36
Jennifer Lee <jenlee.tech@gmail.com>
2020-01-21 23:17:22 -0500

    had to remove Toppings component to resolve error

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    toppings: [
+      {
+        id: 1,
+        ingredient: "nuts",
+        added: false
+      },
+      {
+        id: 2,
+        ingredient: "oreos",
+        added: false
+      },
+      {
+        id: 3,
+        ingredient: "reeses",
+        added: false
+      },
+      {
+        id: 4,
+        ingredient: "gummibears",
+        added: false
+      }
+    ]
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Yummy toppings</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
