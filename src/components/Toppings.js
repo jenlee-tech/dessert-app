d4bae1f07df177af4312e889e5b28149d94feb92
Jennifer Lee <jenlee.tech@gmail.com>
2020-01-22 23:58:22 -0500

    recreated Toppings component with js extension

---
+++ src/components/Toppings.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Toppings extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Toppings </h1>
+      </div>
+    );
+  }
+}
+
+export default Toppings;
